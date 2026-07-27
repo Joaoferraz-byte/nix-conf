8609d5ef730053a0eb2b9e781bb3e55e9bb29a43
joaoferraz-byte <joaoferraz-byte@gmail.com>
2026-07-27 15:34:43 +0000

    fix: Corrigir variável system para pkgs.system no noctalia.nix

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -1,7 +1,7 @@
 { self, inputs, ... }: {
   perSystem = { pkgs, ... }: {
     packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
-      pkgs = pkgs // { noctalia-shell = inputs.wrapper-modules.packages.${system}.noctalia-shell; };
+      pkgs = pkgs // { noctalia-shell = inputs.wrapper-modules.packages.${pkgs.system}.noctalia-shell; };
       settings = builtins.fromJSON (builtins.readFile ./noctalia.json);
     };
   };
