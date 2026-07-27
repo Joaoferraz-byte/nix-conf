6ecae390ced2e2c878bf2e94d3fb705f742668ff
joaoferraz-byte <joaoferraz-byte@gmail.com>
2026-07-27 13:20:54 +0000

    refactor: Etapa 1 - Adicionado Home Manager e organizado módulos

--- modules/hosts/my-machine/default.nix
+++ modules/hosts/my-machine/default.nix
@@ -1,7 +1,16 @@
 { self, inputs, ... }: {
   flake.nixosConfigurations.myMachine = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = { inherit inputs; };
     modules = [
       self.nixosModules.myMachineConfiguration
+      inputs.home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = { inherit inputs; };
+        home-manager.users.livara = import ./home.nix;
+      }
     ];
   };
 }
