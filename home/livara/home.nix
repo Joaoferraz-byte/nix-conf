128ed4902dab709c6d92e92cae1767a21f9b3840
joaoferraz-byte <joaoferraz-byte@gmail.com>
2026-07-28 07:26:43 -0300

    adiciona home/livara (home.nix + neovim.nix)

---
+++ home/livara/home.nix
@@ -0,0 +1,94 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./neovim.nix
+  ];
+
+  home.username = "livara";
+  home.homeDirectory = "/home/livara";
+  home.stateVersion = "24.11";
+
+  home.sessionVariables = {
+    PROJECTS_DIR = "${config.home.homeDirectory}/Projects";
+  };
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    python3Packages.manim
+    nerd-fonts.jetbrains-mono
+  ];
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        padding = { x = 10; y = 10; };
+        dynamic_padding = true;
+        decorations = "none";
+      };
+      font = {
+        normal = { family = "JetBrainsMono Nerd Font"; style = "Regular"; };
+        size = 12.0;
+      };
+      colors = {
+        primary = { background = "#0d1117"; foreground = "#b3b1ad"; };
+        normal = {
+          black = "#484f58"; red = "#ff7b72"; green = "#3fb950"; yellow = "#d29922";
+          blue = "#58a6ff"; magenta = "#bc8cff"; cyan = "#39c5cf"; white = "#b1bac4";
+        };
+        bright = {
+          black = "#6e7681"; red = "#ffa198"; green = "#56d364"; yellow = "#e3b341";
+          blue = "#79c0ff"; magenta = "#d2a8ff"; cyan = "#56d4dd"; white = "#f0f6fc";
+        };
+      };
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = [ "okularApplication_pdf.desktop" ];
+      "application/epub+zip" = [ "com.github.johnfactotum.Foliate.desktop" ];
+      "text/plain" = [ "nvim.desktop" ];
+      "text/x-java" = [ "nvim.desktop" ];
+      "text/x-csrc" = [ "nvim.desktop" ];
+      "text/x-c++src" = [ "nvim.desktop" ];
+      "text/x-python" = [ "nvim.desktop" ];
+      "application/json" = [ "nvim.desktop" ];
+      "text/html" = [ "nvim.desktop" ];
+      "text/css" = [ "nvim.desktop" ];
+      "application/javascript" = [ "nvim.desktop" ];
+    };
+  };
+
+  xdg.desktopEntries.nvim = {
+    name = "Neovim";
+    genericName = "Text Editor";
+    exec = "alacritty -e nvim %F";
+    terminal = false;
+    categories = [ "Utility" "TextEditor" ];
+    mimeType = [ "text/plain" "text/x-java" ];
+  };
+
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    setSessionVariables = true;
+    desktop = "${config.home.homeDirectory}/Desktop";
+    documents = "${config.home.homeDirectory}/Documents";
+    download = "${config.home.homeDirectory}/Downloads";
+    music = "${config.home.homeDirectory}/Music";
+    pictures = "${config.home.homeDirectory}/Pictures";
+    publicShare = "${config.home.homeDirectory}/Public";
+    templates = "${config.home.homeDirectory}/Templates";
+    videos = "${config.home.homeDirectory}/Videos";
+  };
+}
+
