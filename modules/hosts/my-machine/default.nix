b347f4154dff6aec8de7184d9c31e51644b31f98
Joaoferraz-byte <joaoferraz467@gmail.com>
2026-07-29 11:14:09 +0000

    fix: resolve nixvim integration errors with home-manager

--- modules/hosts/my-machine/default.nix
+++ modules/hosts/my-machine/default.nix
@@ -8,12 +8,18 @@
       }
       self.nixosModules.myMachineConfiguration
       inputs.home-manager.nixosModules.home-manager
-      inputs.nixvim.homeModules.nixvim
       {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
         home-manager.extraSpecialArgs = { inherit inputs self; };
         home-manager.backupFileExtension = "backup";
+        # nixvim é importado como módulo compartilhado do Home Manager.
+        # Isso faz com que `programs.nixvim.*` esteja disponível para
+        # todos os usuários do home-manager sem precisar importar
+        # separadamente em cada home.nix.
+        home-manager.sharedModules = [
+          inputs.nixvim.homeModules.nixvim
+        ];
         home-manager.users.livara = import ../../../home/livara/home.nix;
       }
     ];
