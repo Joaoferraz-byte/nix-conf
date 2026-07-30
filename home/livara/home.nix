4bd0c0d94e31217ca1ec4bacaf23f19de6f143c2
joaoferraz-byte <joaoferraz-byte@gmail.com>
2026-07-30 11:04:25 +0000

    fix(nix-conf): corrigir ícone do Neovim no launcher

--- home/livara/home.nix
+++ home/livara/home.nix
@@ -64,6 +64,7 @@
     comment = "Edit text files";
     exec = "${inputs.vim-conf.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/nvim %F";
     terminal = true;
+    icon = "nvim";
     type = "Application";
     mimeType = [
       "text/plain"
