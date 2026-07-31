eca2f562b96ca5de1eaae3418861bf4fe0b740ad
Joaoferraz-byte <joaoferraz467@gmail.com>
2026-07-31 13:37:21 +0000

    chore: ensure clean module structure in ambxst.nix

--- modules/features/ambxst.nix
+++ modules/features/ambxst.nix
@@ -1,22 +1,18 @@
 { self, inputs, ... }: {
   flake.nixosModules.ambxst = { pkgs, config, lib, ... }: {
-    # Importa o módulo NixOS do shell-conf que define as opções do AMBXST
     imports = [ inputs.shell-conf.nixosModules.default ];
 
-    # Configuração do pacote e ativação do serviço no nível do sistema
     programs.ambxst = {
       enable = true;
       package = inputs.shell-conf.packages.${pkgs.stdenv.hostPlatform.system}.default;
     };
 
     # O shell-conf fornece um módulo de Home Manager que gerencia o estado.
-    # Como você usa sharedModules no hyprland.nix, o módulo do shell-conf
-    # deve ser incluído lá ou aqui via sharedModules.
+    # Injetamos ele aqui para garantir que o usuário receba a configuração.
     home-manager.sharedModules = [
       inputs.shell-conf.homeManagerModules.default
     ];
 
-    # Aplicações e dependências do AMBXST
     environment.systemPackages = with pkgs; [
       kitty
       tmux
@@ -29,7 +25,6 @@
       hicolor-icon-theme
     ];
 
-    # Serviços necessários para harmonia do sistema
     services.pipewire = {
       enable = lib.mkDefault true;
       alsa.enable = true;
