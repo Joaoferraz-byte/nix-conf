884cd40f43b167ba6ede6eba8a7cc5433f6448fb
Joaoferraz-byte <joaoferraz467@gmail.com>
2026-08-02 20:29:01 +0000

    fix: remove redundant nixosConfigurations declaration to avoid conflict with local modules

--- modules/parts.nix
+++ modules/parts.nix
@@ -10,11 +10,6 @@
       default = {};
       description = "Home Manager modules exported by this flake.";
     };
-    nixosConfigurations = lib.mkOption {
-      type = lib.types.lazyAttrsOf lib.types.unspecified;
-      default = {};
-      description = "NixOS configurations exported by this flake.";
-    };
   };
 
   config = {
