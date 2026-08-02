75578c8fa44a65cb43c63bc62ab17b26568f9d3c
Joaoferraz-byte <joaoferraz467@gmail.com>
2026-08-02 20:31:39 +0000

    fix: move homeManagerModules declaration to its own file and revert parts.nix to avoid conflicts

--- modules/parts.nix
+++ modules/parts.nix
@@ -1,17 +1,4 @@
-{ lib, ... }: {
-  options.flake = {
-    nixosModules = lib.mkOption {
-      type = lib.types.lazyAttrsOf lib.types.unspecified;
-      default = {};
-      description = "NixOS modules exported by this flake.";
-    };
-    homeManagerModules = lib.mkOption {
-      type = lib.types.lazyAttrsOf lib.types.unspecified;
-      default = {};
-      description = "Home Manager modules exported by this flake.";
-    };
-  };
-
+{
   config = {
     systems = [
       "x86_64-linux"
