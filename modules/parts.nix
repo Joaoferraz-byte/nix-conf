a14bcd0d0a2eebd3c4cf49cd55338cbf447f89f6
Joaoferraz-byte <joaoferraz467@gmail.com>
2026-08-02 20:27:28 +0000

    fix: declare flake output options to allow merging multiple definitions

--- modules/parts.nix
+++ modules/parts.nix
@@ -1,4 +1,22 @@
-{
+{ lib, ... }: {
+  options.flake = {
+    nixosModules = lib.mkOption {
+      type = lib.types.lazyAttrsOf lib.types.unspecified;
+      default = {};
+      description = "NixOS modules exported by this flake.";
+    };
+    homeManagerModules = lib.mkOption {
+      type = lib.types.lazyAttrsOf lib.types.unspecified;
+      default = {};
+      description = "Home Manager modules exported by this flake.";
+    };
+    nixosConfigurations = lib.mkOption {
+      type = lib.types.lazyAttrsOf lib.types.unspecified;
+      default = {};
+      description = "NixOS configurations exported by this flake.";
+    };
+  };
+
   config = {
     systems = [
       "x86_64-linux"
