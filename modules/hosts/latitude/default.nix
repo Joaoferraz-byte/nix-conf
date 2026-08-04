8245eaa534f63237d40c156380be0be5b7f65fe5
Joaoferraz-byte <joaoferraz467@gmail.com>
2026-08-04 09:58:57 +0000

    fix: update flake.lock and add missing overlays to latitude host

--- modules/hosts/latitude/default.nix
+++ modules/hosts/latitude/default.nix
@@ -5,6 +5,14 @@
     modules = [
       {
         nixpkgs.config.allowUnfree = true;
+        nixpkgs.overlays = [
+          (final: prev: {
+            gradience = prev.writeShellScriptBin "gradience" ''
+              echo "gradience foi removido do nixpkgs; stub no-op." >&2
+            '';
+            gnome-icon-theme = prev.adwaita-icon-theme;
+          })
+        ];
       }
       self.nixosModules.latitudeConfiguration
       inputs.shell-conf.nixosModules.dankMaterialShell
