3edee99690d7f719a994ccdc52e03109b3312dd9
Joaoferraz-byte <joaoferraz467@gmail.com>
2026-08-05 11:03:04 -0300

    fix: fix the latitude default.nix sintaxe

--- modules/hosts/latitude/default.nix
+++ modules/hosts/latitude/default.nix
@@ -1,30 +1,45 @@
-{ self, inputs, ... }: {
+{ self, inputs, ... }:
+
+{
   flake.nixosConfigurations.latitude = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
-    specialArgs = { inherit inputs self; };
+
+    specialArgs = {
+      inherit inputs self;
+    };
+
     modules = [
       {
         nixpkgs.config.allowUnfree = true;
+
         nixpkgs.overlays = [
           (final: prev: {
             gradience = prev.writeShellScriptBin "gradience" ''
               echo "gradience foi removido do nixpkgs; stub no-op." >&2
             '';
           })
-        ]
+        ];
       }
+
       self.nixosModules.latitudeConfiguration
       inputs.shell-conf.nixosModules.dankMaterialShell
       inputs.dms-plugin-registry.nixosModules.default
       inputs.home-manager.nixosModules.home-manager
+
       {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = { inherit inputs self; };
+
+        home-manager.extraSpecialArgs = {
+          inherit inputs self;
+        };
+
         home-manager.backupFileExtension = "backup";
+
         home-manager.sharedModules = [
           inputs.nixvim.homeModules.nixvim
         ];
+
         home-manager.users.livara = import ../../../home/livara/home.nix;
       }
     ];
