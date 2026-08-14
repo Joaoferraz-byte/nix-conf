10266c208c978b29f7cf7ab11d747b331ee7ac31
joaoferraz-byte <joaoferraz467@gmail.com>
2026-08-14 18:39:41 +0000

    Fix runtime defaults: Bluetooth and Matugen ownership

--- modules/hosts/common-desktop.nix
+++ modules/hosts/common-desktop.nix
@@ -17,6 +17,14 @@
       };
 
       config = {
+        hardware.bluetooth = {
+          enable = true;
+          powerOnBoot = true;
+          settings.General.Experimental = true;
+        };
+
+        services.blueman.enable = true;
+
         home-manager = {
           useGlobalPkgs = true;
           useUserPackages = true;
