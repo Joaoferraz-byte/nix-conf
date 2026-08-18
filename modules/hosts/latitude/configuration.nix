150feff0073d92925c945c5a6a81c0ae622851f3
joaoferraz-byte <joaoferraz467@gmail.com>
2026-08-18 08:04:01 +0000

    latitude: select zen kernel

--- modules/hosts/latitude/configuration.nix
+++ modules/hosts/latitude/configuration.nix
@@ -15,7 +15,7 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     boot.loader.systemd-boot.configurationLimit = 10;
-    boot.kernelPackages = pkgs.linuxPackages;
+    boot.kernelPackages = pkgs.linuxPackages_zen;
 
     # Network
     networking.hostName = "latitude";
